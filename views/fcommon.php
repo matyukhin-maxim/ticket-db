c6679197967fbd4e63eae4a53ffeccd1f9cad3cd
fellix <matuhin.maxim@gmail.com>
2015-12-31 12:33:37 +0900

    Правильное форматирование всплывающих окон информации об отделе и об ошбках mysql

--- views/fcommon.php
+++ views/fcommon.php
@@ -11,41 +11,43 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div id="info-block" class="alert alert-info">
-				<div class="row">
-					<div class="col-xs-8">
-						<a class="alert-link" href="mailto:kolevatova-ts@dvgk.rao-esv.ru" title="Написать письмо">
-							<i class="glyphicon glyphicon-envelope"></i>
-							Колеватова Т.С.
-						</a>
+			<div class=" col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 col-lg-3 col-lg-offset-5" id="info-block">
+				<div class="alert alert-info">
+					<div class="row">
+						<div class="col-xs-8">
+							<a class="alert-link" href="mailto:kolevatova-ts@dvgk.rao-esv.ru" title="Написать письмо">
+								<i class="glyphicon glyphicon-envelope"></i>
+								Колеватова Т.С.
+							</a>
+						</div>
+						<div class="col-xs-4 text-right strong">
+							<i class="glyphicon glyphicon-earphone"></i>
+							55-88
+						</div>
 					</div>
-					<div class="col-xs-4 text-right strong">
-						<i class="glyphicon glyphicon-earphone"></i>
-						55-88
+					<div class="row">
+						<div class="col-xs-8">
+							<a class="alert-link" href="mailto:matyukhin-mp@dvgk.rao-esv.ru" title="Написать письмо">
+								<i class="glyphicon glyphicon-envelope"></i>
+								Матюхин М.П.
+							</a>
+						</div>
+						<div class="col-xs-4 text-right strong">
+							<i class="glyphicon glyphicon-earphone"></i>
+							51-30
+						</div>
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-8">
-						<a class="alert-link" href="mailto:matyukhin-mp@dvgk.rao-esv.ru" title="Написать письмо">
-							<i class="glyphicon glyphicon-envelope"></i>
-							Матюхин М.П.
-						</a>
-					</div>
-					<div class="col-xs-4 text-right strong">
-						<i class="glyphicon glyphicon-earphone"></i>
-						51-30
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-8">
-						<a class="alert-link" href="mailto:vinogradov-ea@dvgk.rao-esv.ru" title="Написать письмо">
-							<i class="glyphicon glyphicon-envelope"></i>
-							Виноградов Е.А.
-						</a>
-					</div>
-					<div class="col-xs-4 text-right strong">
-						<i class="glyphicon glyphicon-earphone"></i>
-						50-98
+					<div class="row">
+						<div class="col-xs-8">
+							<a class="alert-link" href="mailto:vinogradov-ea@dvgk.rao-esv.ru" title="Написать письмо">
+								<i class="glyphicon glyphicon-envelope"></i>
+								Виноградов Е.А.
+							</a>
+						</div>
+						<div class="col-xs-4 text-right strong">
+							<i class="glyphicon glyphicon-earphone"></i>
+							50-98
+						</div>
 					</div>
 				</div>
 			</div>
