635c3acf71dcbe2e810f7852dd19b0c8b6387f09
fellix <matuhin.maxim@gmail.com>
2016-01-11 10:25:17 +0900

    Исправил ошибку замены параметра переданного в качестве массива. При передаче в запрос двух такх параметров, счетчик не обнулялся, и добавлялась лишняя запятая

--- core/CModel.php
+++ core/CModel.php
@@ -67,15 +67,17 @@ class CModel {
 		foreach ($param as $key => $value) {
 			if (gettype($value) === 'array') {
 				$condition = "(";
+				$local = 0;
 				foreach ($value as $item) {
 
-					$condition .= $cnt ? "," : ""; // если не первый параметр, то добавим запятую
+					$condition .= $local ? "," : ""; // если не первый параметр, то добавим запятую
 					$vparam = "_X" . ++$cnt;
 					$condition .= " :$vparam";
 
 					// а параметр подмассива перекидываем в основной массив
 					// елементы вложенного массива не должны быть сами массивами, иначе хрень будет
 					$param[$vparam] = $item;
+					$local++;
 				}
 				$condition .= ") ";
 				$query = str_replace(":$key", $condition, $query);
@@ -114,30 +116,6 @@ class CModel {
 			self::$errorlist[] = "MySQL error [$ecode]: " . ($emsg ? $emsg : 'Invalid params');
 		}
 		return $sth->fetchAll();
-	}
-
-	protected function prepareArguments($parguments, &$query) {
-		$res = [];
-		$cnt = 0;
-		foreach ($parguments as $key => $value) {
-			if (gettype($value) === 'array') {
-				$condition = "(";
-				foreach ($value as $item) {
-
-					$condition .= $cnt ? "," : ""; // если не первый параметр, то добавим запятую
-					$vparam = "_X" . ++$cnt;
-					$condition .= " :$vparam";
-
-					// а параметр подмассива перекидываем в основной массив
-					// елементы вложенного массива не должны быть сами массивами, иначе хрень будет
-					$res[$vparam] = $item;
-				}
-				$condition .= ") ";
-				$query = str_replace(":$key", $condition, $query);
-			} else
-				$res[$key] = $value;
-		}
-		return $res;
 	}
 
 	public static function isConnected() {
