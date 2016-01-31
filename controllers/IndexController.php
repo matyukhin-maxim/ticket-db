5d948b32b7ae65294c5f6abde002e01b4f46b9c2
fellix <matuhin.maxim@gmail.com>
2016-01-31 14:59:16 +0900

    Удаленная авторизация

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -4,6 +4,12 @@ class IndexController extends CController {
 
 	public function actionIndex() {
 
+		$sid = get_param($_REQUEST, 'PHPSESSID');
+		if ($sid) {
+
+			Session::destroy();
+			setcookie("PHPSESSID", $sid, time() + 3600 * 2, '/');
+		}
 		$this->redirect('/contents/');
 	}
 
