120eb21b664b94cb6dff0b7edbf2e7b15837281a
fellix <matuhin.maxim@gmail.com>
2016-02-16 09:31:23 +0900

    Телефоны в помощь

--- views/about/help.php
+++ views/about/help.php
@@ -1,14 +1,52 @@
 <div class="row">
 	<div class="col-sm-10 col-sm-offset-1">
 		<br/><br/><br/>
-		<div class="alert alert-info">
-			<br/><br/><br/>
+		<div class="alert alert-warning">
+			<br/><br/>
 
 			<h1>Журнал заявок НГРЭС</h1>
-			<blockquote>
-				ТУТ ПОЗЖЕ БУДЕТ ИНСТРУКЦИЯ
-			</blockquote>
-<!--			<a href="/transfer/" class="btn btn-danger">НЕ НАЖИМАТЬ</a>-->
+
+			<div class="alert alert-danger text-center strong">
+				По всем вопросами можно звонить или писать по указаным ниже контактным данным.
+			</div>
+			<h3>
+				<div class="row">
+					<div class="col-xs-8">
+						<a class="alert-link" href="mailto:kolevatova-ts@dvgk.rao-esv.ru" title="Написать письмо">
+							<i class="glyphicon glyphicon-envelope"></i>
+							Колеватова Т.С.
+						</a>
+					</div>
+					<div class="col-xs-4 text-right strong">
+						<i class="glyphicon glyphicon-earphone"></i>
+						55-88
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-8">
+						<a class="alert-link" href="mailto:matyukhin-mp@dvgk.rao-esv.ru" title="Написать письмо">
+							<i class="glyphicon glyphicon-envelope"></i>
+							Матюхин М.П.
+						</a>
+					</div>
+					<div class="col-xs-4 text-right strong">
+						<i class="glyphicon glyphicon-earphone"></i>
+						51-30
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-8">
+						<a class="alert-link" href="mailto:vinogradov-ea@dvgk.rao-esv.ru" title="Написать письмо">
+							<i class="glyphicon glyphicon-envelope"></i>
+							Виноградов Е.А.
+						</a>
+					</div>
+					<div class="col-xs-4 text-right strong">
+						<i class="glyphicon glyphicon-earphone"></i>
+						50-98
+					</div>
+				</div>
+			</h3>
 			<br/><br/><br/>
 		</div>
 	</div>
